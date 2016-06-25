680f60e787be33610964d16a7b38419309f5b0a2
Christel Trutmann & Carrie Liang <pair+ctrutmann.liangcarrie20@devbootcamp.com>
2016-06-25 16:46:23 -0700

    Add AMAZING animations

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  // $('#red1').on('click', function() {
+  //   animate({ marginTop: '+=200' }, 1500, function() {
+  //     bounce($(this));
+  //   });
+  // });
+  $('.column').on('click', function() {
+    var $piece = $(this).children('.piece')
+    $($piece).removeClass('hidden');
+    $($piece).animate({ marginTop: '+=305' }, 1500, function() {
+      bounce($(this));
+    });
+  });
+
+
+  function bounce(element) {
+    for(i = 0; i < 3; i++) {
+      element.animate( {marginTop: '-=20'}, 200)
+      .animate({marginTop: '+=20'}, 200)
+    }
+  }
+
 });
