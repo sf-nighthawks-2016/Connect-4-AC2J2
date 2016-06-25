877020289ec005e448f58fd1b6557b5789d6cfd1
Jason Sutter <email": null>
2016-06-25 14:20:25 -0700

    add newGame function

--- public/js/connect_4.js
+++ public/js/connect_4.js
@@ -1,40 +1,47 @@
-var gameBoard =
-[
-[0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0]
-];
+var gameBoard;
+var currentPlayer;
 
-var currentPlayer = "red";
-
-$(document).ready(function(){
-  newGame();
-})
+// $(document).ready(function() {
+//   newGame();
+// });
 
 function newGame(){
-
+  rowMax = 6;
+  colMax = 7;
+  for (var row = 0; row < rowMax; row++) {
+        var gameBoard = [];
+        for(var col = 0; col < colMax; col++) {
+            gameBoard[row][col] = 0;
+            console.log(newTimes);
+        }
+      }
+  currentPlayer = "red";
 }
+
 function whoWon(){
 
 }
+
 function whoesTurn(){
 
 }
+
 function placePiece(){
 
 }
+
 function endGame(){
   alert( currentPlayer + "wins!" );
 }
+
 function checkCol(){
 
 }
+
 function checkRow(){
 
 }
+
 function transposeBoard(board){
 
 }
