e2cab56a304bc896da2dc03e7067985d3466ca27
Jason Sutter <email": null>
2016-06-25 14:03:29 -0700

    add gameBoard array variable

--- public/js/connect_4.js
+++ public/js/connect_4.js
@@ -1,13 +1,41 @@
+var gameBoard =
+[
+[0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0]
+];
+
+var currentPlayer = "red";
+
 $(document).ready(function(){
   newGame();
 })
 
-function newGame(){}
-function whoWon(){}
-function whoesTurn(){}
-function placePiece(){}
-function endGame(){}
-function checkCol(){}
-function checkRow(){}
-function transposeBoard(board){}
+function newGame(){
+
+}
+function whoWon(){
+
+}
+function whoesTurn(){
+
+}
+function placePiece(){
+
+}
+function endGame(){
+  alert( currentPlayer + "wins!" );
+}
+function checkCol(){
+
+}
+function checkRow(){
+
+}
+function transposeBoard(board){
+
+}
 
